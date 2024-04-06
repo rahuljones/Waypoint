12134247b66413d9b11cbed979e8d4a8833d8a3a
rahuljones <rahuljjones@gmail.com>
2024-04-06 12:07:18 -0700

    Adding in starter code.

---
+++ waypoint/backend/index.js
@@ -0,0 +1,31 @@
+import { useId } from 'react';
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const router = require('./routes/router')
+
+const app = express()
+
+let map = {};
+
+app.use(express.json());
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+
+
+app.get('/api/get/:id', (req, res)=> {
+    const id = req.params.id; // Get the 'id' parameter from the URL
+    res.send(map[id]);
+})
+
+app.post('/api/insert', (req, res) => {
+    let obj = req.body.stepsData;
+    const generatedId = useId();
+    map[generatedId] = obj;
+    const shareLink = "http://localhost:3000/response/" + lastInsertId;
+    console.log("In the backend:" + shareLink);
+    res.send({ "link": shareLink });
+})
+app.listen(3001, () => {
+    console.log("running on port 3001");
+})
