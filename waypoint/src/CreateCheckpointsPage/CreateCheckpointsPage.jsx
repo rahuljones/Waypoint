b86a2d7d156be5cbc3b87da5f284933e4f9c5ee8
SammyoRoy <sammyo.roychowdhury@gmail.com>
2024-04-06 12:43:43 -0700

    Created first screen

---
+++ waypoint/src/CreateCheckpointsPage/CreateCheckpointsPage.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function CreateCheckpointsPage(){
+
+    return(
+        <div className="Background">
+            <h2 className="Title">Waypoint</h2>
+            <div className="InputFrame">
+                
+            </div>
+            <div className="CheckPointList">
+
+            </div>
+            
+        </div>
+    );
+}
+
+export default CreateCheckpointsPage;
