55e2c8eb619fee2372221d0d9513e77089f388e1
SammyoRoy <sammyo.roychowdhury@gmail.com>
2024-04-06 14:09:40 -0700

    Create checkpoints page functionality complete

---
+++ waypoint/src/CreateCheckpointsPage/CheckPointListElement.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CheckpointListElement({ checkpoint }) {
+    return (
+        <div className="CheckpointListItem">
+            {checkpoint}
+        </div>
+    );
+}
+
+export default CheckpointListElement;
